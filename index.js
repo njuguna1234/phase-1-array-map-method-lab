9f7677ce2b7e5d9eb2d354f0b56813cd6cc160fd
roy wahome <roy.njuguna@student.moringaschool.com>
2024-03-29 12:18:40 +0300

    the title case

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = ()=> {return tutorials.map(title => {
+  return title
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+});
+};
+
+module.exports = {
+titleCased: titleCased
 }
+
